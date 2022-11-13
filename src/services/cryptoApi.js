54f1c52c1518b185aa05d04d20999e0edef7d355
Ryland Adams <rylandadams@yahoo.com>
2022-11-13 12:52:10 -0800

    redux store setup

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '1da661cc5cmshd340378e70cd969p129eccjsn8826bc4ec3e3',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// };
