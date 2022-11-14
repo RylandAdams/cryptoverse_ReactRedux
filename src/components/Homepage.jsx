c5f144644fc322b696766d6ad80bcc6191d71bbe
Ryland Adams <rylandadams@yahoo.com>
2022-11-14 12:32:17 -0800

    crypto news refined + filter search for specific crypto

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -33,7 +33,7 @@ const Homepage = () => {
     <Cryptocurrencies simplified />
     <div className='home-heading-container'>
       <Title level={2} className='home-title'>Latest Crypto News</Title>
-      <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
+      <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
     </div>
     <News simplified />
     </>
