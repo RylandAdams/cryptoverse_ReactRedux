1865e4231fd3ab7fa347e3141fc691b6c4de740b
Ryland Adams <rylandadams@yahoo.com>
2022-11-13 17:07:48 -0800

    filtering by text search

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
 
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const count = simplified ? 10 : 100
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(cryptos)
+  //this function will run when ever either of these change [cryptosList, searchTerm] 
+  //Will also run when component mounts
+  useEffect(() => {
+
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
+    setCryptos(filteredData)
+
+  }, [cryptosList, searchTerm]);
+
+  if(isFetching) return 'Loading...'
 
 
   return (
     <>
+      <div className='seach-crypto'>
+        <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
       <Row gutter={[32, 32]} className='crypto-card-container'>
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
               <Link to={`/crypto/${currency.id}`}>
                 <Card 
